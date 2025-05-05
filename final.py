a64e934ea947dcb014db984207007d80c44ecd8f
ntnc123 <ngoccham299205@gmail.com>
2025-05-05 22:52:25 +0700

    Update file final.py

--- final.py
+++ final.py
@@ -1,6 +1,14 @@
-content = '''
-Quang Dũng là một người nghệ sĩ đa tài, ông là một nhạc sĩ, một họa sĩ, đồng thời cũng là một thi sĩ. Chính vì vậy mà trong những vần thơ của Quang Dũng, ta thấy có cả âm hưởng của nhạc và họa. Ông được mệnh danh là “bóng mây qua đỉnh Việt”, với hồn thơ bay bổng, phóng khoáng, hào hoa đầy lãng mạn và tha thiết tình cảm dành cho bạn bè, quê hương xứ sở. Dù viết ít, viết theo cảm hứng nhưng ông đã thực sự thành công khi viết về người lính, và “Tây Tiến” là bài thơ tiên phong cho phong cách thơ Quang Dũng, đồng thời cũng là đứa con tinh thần tráng kiện và hào hoa, là đỉnh cao trong sự nghiệp sáng tác của ông. Với hai đặc điểm nổi bật là cảm hứng lãng mạn và tinh thần bi tráng trong bài thơ, Quang Dũng đã khắc họa thành công hình tượng người lính Tây Tiến trên cái nền cảnh thiên nhiên núi rừng miền Tây hùng vĩ, tráng lệ.
-Tây Tiến là tên của một đơn vị quân đội thành lập vào năm 1947, có nhiệm vụ phối hợp với bộ đội Lào, bảo vệ biên giới Việt – Lào và đánh tiêu hao lực lượng quân đội Pháp ở Thượng Lào cũng như miền Tây Bắc Bộ Việt Nam. Đoàn quân Tây Tiến sau một thời gian hoạt động ở Lào đã trở về Hòa Bình thành lập trung đoàn 52. Cuối năm 1948, Quang Dũng chuyển sang đơn vị khác, và cũng chính thời gian này, bài thơ “Tây Tiến” ra đời. “Tây Tiến” được viết trong niềm thương nỗi nhớ đơn vị cũ, chiến trường xưa. Ban đầu tác phẩm có tên là “Nhớ Tây Tiến”, sau này khi in trong tập “Mây đầu ô”, tác giả đổi lại thành “Tây Tiến”. Toàn bài thơ bao trùm bởi những nỗi nhớ: đó là nỗi nhớ về những chặng đường hành quân, nỗi nhớ về những kỉ niệm nơi miền Tây, nỗi nhớ về binh đoàn Tây Tiến anh hùng và nỗi nhớ về lời thề của binh đoàn Tây Tiến.
+content = '''Nội dung cốt lõi Tư tưởng Hồ Chí Minh
+Tư tưởng về giải phóng dân tộc, giải phóng giai cấp, giải phóng con người;
+Độc lập dân tộc gắn liền với chủ nghĩa xã hội; kết hợp sức mạnh dân tộc với sức mạnh thời đại;
+Sức mạnh của nhân dân, của khối đại đoàn kết dân tộc;
+Quyền làm chủ của nhân dân, xây dựng Nhà nước thật sự của dân, do dân và vì dân
+Quốc phòng toàn dân, xây dựng lực lượng vũ trang nhân dân;
+Về phát triển kinh tế, văn hóa, không ngừng nâng cao đời sống vật chất và tinh thần của nhân dân;
+Về phát triển kinh tế và văn hóa, không ngừng nâng cao đời sống vật chất và tinh thần của nhân dân;
+Đạo đức cách mạng, cần, kiệm, liêm, chính, chí công vô tư;
+Chăm lo bồi dưỡng thế hệ cách mạng cho đời sau;
+Xây dựng Đảng trong sạch, vững mạnh, cán bộ, Đảng viên vừa là lãnh đạo, vừa là người đầy tớ thật trung thành của nhân dân...
 '''
 
 
@@ -20,7 +28,7 @@ sentences = nltk.sent_tokenize(contents_parsed)
 
 #Bước 3: Tách từ trong câu và tính toán vector cho từng câu
 from gensim.models import KeyedVectors
-w2v = KeyedVectors.load_word2vec_format(r"D:\gitab\vi_txt\vi.vec")
+w2v = KeyedVectors.load_word2vec_format(r"D:\gitab\vi_txt\vi.vec", binary=False)
 vocab = w2v.key_to_index  # Danh sách các từ trong từ điển
 from pyvi import ViTokenizer
 
@@ -37,7 +45,7 @@ for sentence in sentences:
 
 #Bước 4: Phân cụm câu băng KMeans
 from sklearn.cluster import KMeans
-n_clusters = 5 #Sô lượng cụm (có thể thay đổi)
+n_clusters = 3 #Sô lượng cụm (có thể thay đổi)
 kmeans = KMeans(n_clusters=n_clusters)
 kmeans = kmeans.fit(X)
 
