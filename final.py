b7faa5b17e43c54f7834fb4f2d81b54b162f4ec6
ntnc123 <ngoccham299205@gmail.com>
2025-05-08 08:36:52 +0700

    Update file final.py

--- final.py
+++ final.py
@@ -1,24 +1,25 @@
 #THƯ VIỆN
-#pip install nltk
-# pip install gensim
-# pip install sklearn
-# pip install pyvi
-# pip install fasttext
-# pip install numpy==1.26.4
+# pip install nltk #Sử dụng để tách câu
+# pip install gensim #Sử dụng để tách từ tiếng Việt
+# pip install sklearn #Phân cụm câu và chọn câu đại diện
+# pip install pyvi #Tải mô hình Word2Vec và truy xuất vector từ
+# pip install numpy==1.26.4 #Tính toán trung bình vector câu
 
-content = '''Nội dung cốt lõi Tư tưởng Hồ Chí Minh
-Tư tưởng về giải phóng dân tộc, giải phóng giai cấp, giải phóng con người;
-Độc lập dân tộc gắn liền với chủ nghĩa xã hội; kết hợp sức mạnh dân tộc với sức mạnh thời đại;
-Sức mạnh của nhân dân, của khối đại đoàn kết dân tộc;
-Quyền làm chủ của nhân dân, xây dựng Nhà nước thật sự của dân, do dân và vì dân
-Quốc phòng toàn dân, xây dựng lực lượng vũ trang nhân dân;
-Về phát triển kinh tế, văn hóa, không ngừng nâng cao đời sống vật chất và tinh thần của nhân dân;
-Về phát triển kinh tế và văn hóa, không ngừng nâng cao đời sống vật chất và tinh thần của nhân dân;
-Đạo đức cách mạng, cần, kiệm, liêm, chính, chí công vô tư;
-Chăm lo bồi dưỡng thế hệ cách mạng cho đời sau;
-Xây dựng Đảng trong sạch, vững mạnh, cán bộ, Đảng viên vừa là lãnh đạo, vừa là người đầy tớ thật trung thành của nhân dân...
+content = '''
+Tối nào cũng vậy, cứ đến lúc con bé lớn ông Hai thu que đóm cháy lập lòe trong chiếc nón rách tất tả đi từ nhà bếp lên, và bà Hai ngồi ngây thuỗn cái mặt trước đĩa đèn dầu lạc, lầm bầm tính toán những tiền cua, tiền bún, tiền chuối, tiền kẹo… thì ông Hai vùng dậy, sang bên bác Thứ nói chuyện. Không hiểu sao cứ đến lúc ấy ông Hai lại thấy buồn. Nằm nghe tiếng súng dội trong đêm tối và nhất là cái tiếng rì rầm tính toán tiền nong của mụ vợ, tự nhiên ông sinh ra nghĩ ngợi vẩn vơ, nó bực dọc làm sao ấy. Mà ông, thì không thích nghĩ ngợi như thế một tí nào. Ông vốn là người hay làm, ở quê ông làm suốt ngày, không mấy lúc chịu ngơi chân ngơi tay. Không đi cày đi cuốc, không gánh phân tát nước thì ông cũng phải bày vẽ ra công việc gì để làm: đan rổ, đan rá hay chữa cái chuồng gà, cạp lại tấm liếp. Từ ngày tản cư lên đây, suốt ngày mấy bố con nhong nhóng ngồi ăn, tối đến lại nghe những tiếng rì rầm tính toán ấy, ruột gan ông cứ nóng lên như lửa đốt. Ông phải đi chơi cho khuây khỏa. Lần nào cũng như lần nào, cứ vừa nhô đầu qua cái mái lá bên gian bác Thứ là ông lão hỏi ngay: “Thế nào, hôm nay có gì không bác?”...
 '''
 
+# Đọc nội dung từ file
+# from docx import Document
+
+# def read_docx(file_path):
+#     doc = Document(file_path)
+#     full_text = []
+#     for paragraph in doc.paragraphs:
+#         full_text.append(paragraph.text)
+#     return '\n'.join(full_text)\
+    
+# content = read_docx(r"D:\gitab\TT3601_NguyenThiNgocCham_BaiChinhLuan.docx")
 
 #Bước 1: Tiền xử lý văn bản
 contents_parsed = content.lower() #Biến đổi hết thành chữ thường
@@ -56,7 +57,7 @@ for sentence in sentences:
 
 #Bước 4: Phân cụm câu bằng KMeans
 from sklearn.cluster import KMeans
-n_clusters = 3 #Sô lượng cụm (có thể thay đổi)
+n_clusters = 5 #Sô lượng cụm (có thể thay đổi)
 kmeans = KMeans(n_clusters=n_clusters)
 kmeans = kmeans.fit(X)
 
@@ -78,3 +79,4 @@ summary = ' '.join([sentences[closest[idx]] for idx in ordering])
 
 #Bước 6: In ra kết quả
 print("\nTóm Tắt:\n",summary)
+
